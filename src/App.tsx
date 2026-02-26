8a64ac09ec2e2447c080e622f2e8b11d709b01a6
fff897 <thummatipwork@gmail.com>
2026-02-26 19:09:02 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Change BrowserRouter to HashRouter
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-// 1. Import your pages
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-//survey
-import HairlossReg from "./pages/Survey/Hairloss_reg"; // Added this import
+import HairlossReg from "./pages/Survey/Hairloss_reg";
 import SexualReg from "./pages/Survey/SexualHealth_reg";
 import WeightlossReg from "./pages/Survey/Weightloss_reg";
 import BodyHealthReg from "./pages/Survey/BodyHealth_reg";
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -19,19 +19,23 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/medicine">
+      {/* 1. Switched to HashRouter. 
+          2. Removed 'basename' as HashRouter handles the repo path automatically.
+      */}
+      <HashRouter>
         <Routes>
-          
           <Route path="/" element={<Index />} />
           
-          
+          {/* Survey Routes */}
           <Route path="/Survey/Hairloss_reg" element={<HairlossReg />} />
           <Route path="/Survey/SexualHealth_reg" element={<SexualReg />} /> 
           <Route path="/Survey/Weightloss_reg" element={<WeightlossReg />} />
           <Route path="/Survey/BodyHealth_reg" element={<BodyHealthReg />} />
+          
+          {/* 404 Catch-all */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
