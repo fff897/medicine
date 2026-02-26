18d1ce012e612ba713341a5b1a6644b9235e6c85
fff897 <thummatipwork@gmail.com>
2026-02-26 18:23:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/medicine", // <--- ADD THIS LINE
+  base: "/medicine/", // <--- ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
